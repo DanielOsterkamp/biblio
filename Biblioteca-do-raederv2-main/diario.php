6b2508b7e9fbd4cfece951714503f74e7d889ec2
DanielOsterkamp <DanielOsterkamp.ss038@academico.ifsul.edu.br>
2025-10-02 19:27:34 -0300

    quantidadeDeCopias

--- Biblioteca-do-raederv2-main/diario.php
+++ Biblioteca-do-raederv2-main/diario.php
@@ -132,13 +132,50 @@ input[type="text"]:focus {
 O livro mostra de forma leve e humorada os desafios de crescer, lidar com colegas populares, professores, irmãos irritantes e a vontade de ser aceito.</td>
                </tr>
                 
+              <?php
+
+              $quantidadeDeCopias = 5;
+              $id = 6;
+
+              echo "<tr> <td>Quantidade de Cópias: ".$quantidadeDeCopias ."</td>";
+
+              
+
+                include_once('config.php');
+
+                $stmt = $conexao->prepare("SELECT count(*) FROM Emprestimo WHERE id_livro = ?");
+                if (!$stmt) {
+                    die("Erro no prepare: " . $conexao->error);
+                }
+
+                $stmt->bind_param("s", $id); // mudar o "sss" para quantidade de variaveis
+
+                if (!$stmt->execute()) {
+                    die("Erro no execute: " . $stmt->error);
+                }
+
+                $result = $stmt->get_result();
+
+          
+
+                if ($result->num_rows > 0){
+              
+                 
+                    echo  "<td>Quantidade de Cópias Disponiveis: ". $quantidadeDeCopias- (int)$result->fetch_assoc(). "</td> </tr>";
+                }else{
+                 echo "<td>Quantidade de Cópias Disponiveis: ". $quantidadeDeCopias. "</td> </tr>";
+                } 
+
+                ?>
+
+            
+
+
+
+
                <tr>
-                <td>Quantidade de Cópias: 5</td>
-                 <td>Quantidade de Cópias Disponiveis: 1</td>
-               </tr>
-               <tr>
-                <td > <button style="padding: 15px 40px; font-size: 15px;" > Devolver</button></td>
-                <td > <button style="padding: 15px 40px; font-size: 15px;" > Pegar </button></td>
+                <td > <form action="devolverLivro.php" method="post"><button style="padding: 15px 40px; font-size: 15px;" > Devolver</button></form></td>
+                <td > <form action="pegarLivro.php" method="post"></form><button style="padding: 15px 40px; font-size: 15px;" > Pegar </button></form></td>
                </tr>
                  
 
